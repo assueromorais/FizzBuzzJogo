cc39f0e460b91abddd3fd6756229e71c4c39d5e2
ASSUÉRO ARAÚJO MORAIS <assueromorais@hotmail.com>
2018-03-02 21:58:35 -0300

    Refatoramento do código com melhorias sugeridas em avaliação de código realizada em 24/02/18.

--- FizzBuzzJogo.Tests/Tests.fs
+++ FizzBuzzJogo.Tests/Tests.fs
@@ -6,39 +6,33 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodFail () = 
-        Assert.IsTrue(false);
-
-    [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
-    
-    [<TestMethod>]
-    member this.TestFizzBuzz21Pass () =
-        let esperado = [ "1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"Fizz";"Buzz";"11";"Fizz";"13";"14";"FizzBuzz";"16";"17";"Fizz";"19";"Buzz";"Fizz"]
+    member __.``Testar FizzBuzz 21 - Passa``() =
+        let esperado = [ "1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"Fizz";"Buzz";"11";"Fizz";"13"; "14"; "FizzBuzz"; "16";"17";"Fizz";"19";"Buzz";"Fizz"]
         let atual = FizzBuzzJogo.FizzBuzz(21)
         Assert.AreEqual<list<string>>(esperado, atual);
     
     [<TestMethod>]
-    member this.TestFizzBuzz9Fail () =
+    member __.``Testar FizzBuzz 9 - Falha`` () =
         let esperado = ["1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"9"]
         let atual = FizzBuzzJogo.FizzBuzz(9)
+        Assert.AreNotEqual<list<string>>(esperado, atual);
+    
+    [<TestMethod>]
+    member __.``Testar FizzBuzz 21 - V2 - Passa``() =
+        let regras = [(fun i -> if i % 15 = 0 then "FizzBuzz" else null); 
+                        (fun i -> if i % 3 = 0 && i % 5 <> 0 then "Fizz" else null); 
+                        (fun i -> if i % 5 = 0 && i % 3 <> 0 then "Buzz" else null); 
+                        (fun i -> if i % 3 <> 0 && i % 5 <> 0 then string i else null)]
+        let esperado = [ "1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"Fizz";"Buzz";"11";"Fizz";"13"; "14"; "FizzBuzz"; "16";"17";"Fizz";"19";"Buzz";"Fizz"]
+        let atual = FizzBuzzJogo2.FizzBuzz 21 regras
         Assert.AreEqual<list<string>>(esperado, atual);
     
     [<TestMethod>]
-    member this.TestFizzBuzzV221Pass () =
-        let triggers = [(fun i -> if i % 15 = 0 then "FizzBuzz" else null); (fun i -> if i % 3 = 0 && i % 5 <> 0 then "Fizz" else null); (fun i -> if i % 5 = 0 && i % 3 <> 0 then "Buzz" else null); (fun i -> if i % 3 <> 0 && i % 5 <> 0 then string i else null)]
-        let esperado = [ "1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"Fizz";"Buzz";"11";"Fizz";"13";"14";"FizzBuzz";"16";"17";"Fizz";"19";"Buzz";"Fizz"]
-        let atual = FizzBuzzJogo2.FizzBuzz 21 triggers
-        Assert.AreEqual<list<string>>(esperado, atual);
-    
-    [<TestMethod>]
-    member this.TestFizzBuzzV29Fail () =
-        let triggers = [(fun i -> if i % 15 = 0 then "FizzBuzz" else null); 
+    member __.``Testar FizzBuzz 9 - V2 - Falha``() =
+        let regras = [(fun i -> if i % 15 = 0 then "FizzBuzz" else null); 
                         (fun i -> if i % 3 = 0 && i % 5 <> 0 then "Fizz" else null); 
                         (fun i -> if i % 5 = 0 && i % 3 <> 0 then "Buzz" else null); 
                         (fun i -> if i % 3 <> 0 && i % 5 <> 0 then string i else null)]
         let esperado = ["1";"2";"Fizz";"4";"Buzz";"Fizz";"7";"8";"9"]
-        let atual = FizzBuzzJogo2.FizzBuzz 9 triggers
-        Assert.AreEqual<list<string>>(esperado, atual);
-    
+        let atual = FizzBuzzJogo2.FizzBuzz 9 regras
+        Assert.AreNotEqual<list<string>>(esperado, atual);
--- FizzBuzzJogo/FizzBuzzJogo2.fs
+++ FizzBuzzJogo/FizzBuzzJogo2.fs
@@ -1,22 +1,12 @@
-﻿// Fornece os métodos necessários para o jogo FizzBuzz com opção de encaminhar as funções com as regras da contagem.
-module FizzBuzzJogo2
+﻿module FizzBuzzJogo2
 
-//  Processa as regras de contagem recebidas, retornando o text conforme o valor atual.
 let Contar valor regras : string = 
-    let mutable resultado = null
-    regras |> List.iter (fun r -> 
-                if (r valor <> null && resultado = null) then 
-                    resultado <- r valor  // Altera para o valor retornado pela função.
-                else 
-                    resultado <- resultado) // Mantém o valor.
-    if(resultado <> null) then // Uma das funções retornou um valor.
-        resultado
-    else
-        string valor
+    regras 
+    |> List.map (fun r -> r valor)
+    |> List.filter (fun x -> not (isNull x))
+    |> List.head
     
-// Executa o jogo FizzBuzz, até o valor de max.
-// O parâmetro regras contém as funções que tratam os padrões de contagem.
+/// Executa o jogo FizzBuzz, até o valor de max.
+/// Parâmetro regras contém as funções que tratam os padrões de contagem.
 let FizzBuzz max regras: string list =
-      // Percorre a lista gerando um novo array com os valores correspondentes
-      List.init max (fun index -> Contar (index + 1) regras)
-      
+    List.init max (fun index -> Contar (index + 1) regras)
